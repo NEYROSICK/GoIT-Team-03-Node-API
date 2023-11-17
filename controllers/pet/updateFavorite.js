3f736012b7e18461822b7594c02335283a04cdad
Z1NG0D <YaroslavBorovskiy@gmail.com>
2023-11-17 23:02:34 +0100

    comit

--- controllers/pet/updateFavorite.js
+++ controllers/pet/updateFavorite.js
@@ -3,15 +3,10 @@ const { User } = require("../../models/user");
 const updateFavorite = async (req, res) => {
   const { userId } = req.user;
   const { petId } = req.body;
-    
-  const updatedUser = await User.updateOne(
-    userId,
-    { $push: { favoritesArr: petId } }
-  );
 
-  res
-    .status(200)
-    .json({ message: "Added to favorites successfully", user: updatedUser });
+  await User.updateOne(userId, { $push: { favoritesArr: petId } });
+
+  res.status(200).json({ message: "Added to favorites successfully" });
 };
 
 module.exports = updateFavorite;
