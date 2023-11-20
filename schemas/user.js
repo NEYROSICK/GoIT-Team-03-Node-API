62ba67124bc183c1274e35db70c73a76a7185e73
Иван-Т0 <vanyataran24@gmail.com>
2023-11-20 18:52:44 +0200

    update

--- schemas/user.js
+++ schemas/user.js
@@ -34,14 +34,6 @@ const usersSchema = Joi.object({
   email: Joi.string()
     .email()
     .message("Email should have a format like: email@email.com"),
-  password: Joi.string()
-    .min(6)
-    .max(16)
-    .pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])/)
-    .message(
-      "Invalid password format. Please make sure your password: is at least 6 characters long, is at most 16 characters long, - contains at least one digit, contains at least one lowercase letter, contains at least one uppercase letter."
-    )
-    .required(),
   date: Joi.string()
     .pattern(/^\d{2}-\d{2}-\d{4}$/)
     .messages({
